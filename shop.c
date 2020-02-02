97bea8531b0df675c0f703ceffd7d5c9b541a7df
Stévenn Guégan <59653320+ElMonsieurp@users.noreply.github.com>
2020-02-02 21:45:35 +0100

    Add files via upload
    Amelioration de la vérification pour l'achat d'objets.

--- shop.c
+++ shop.c
@@ -47,34 +47,54 @@ if (choix==1){
  while (choixA!=10 && argent>0) {
  printf ("Que voulez vous achetez? 1= Epee , 2= Bombe, 3= Soin 4= Fleches 5= Arc 10= Quitter \n");
  scanf("%d",&choixA);
- if (choixA==1) {
+ if (choixA==1 && argent>=200) {
  argent=argent-shop[0];
  epee=epee+1;
  printf("Argent : %d \n",argent);
  choixA=0;
  }
- if (choixA==2) {
+ else if (choixA==1 && argent<200) {
+ printf("Vous n'avez pas assez de Golds \n Argent : %d \n",argent); 
+ choixA=0;
+ }
+ if (choixA==2 && argent>=50) {
  argent=argent-shop[1];
  bombe=bombe+1;
 printf("Argent : %d \n",argent); 
  choixA=0;
  }
- if (choixA==3) {
+  else if (choixA==2 && argent<50) {
+ printf("Vous n'avez pas assez de Golds \n Argent : %d \n",argent); 
+ choixA=0;
+ }
+ if (choixA==3 && argent>=30) {
  argent=argent-shop[2];	 
  soin=soin+1;
  printf("Argent : %d \n",argent);
   choixA=0;
  }
- if (choixA==4) {
+  else if (choixA==3 && argent<30) {
+ printf("Vous n'avez pas assez de Golds \n Argent : %d \n",argent); 
+ choixA=0;
+ }
+ if (choixA==4 && argent>=25) {
  argent=argent-shop[3];	
 fleche=fleche+1;
  printf("Argent : %d \n",argent);
   choixA=0;
  }
- if (choixA==5) {
+  else if (choixA==4 && argent<25) {
+ printf("Vous n'avez pas assez de Golds \n Argent : %d \n",argent); 
+ choixA=0;
+ }
+ if (choixA==5 && argent>=300) {
  argent=argent-shop[4];	
  arc=arc+1;
 printf("Argent : %d \n",argent); 
+ choixA=0;
+ }
+  else if (choixA==5 && argent<300) {
+ printf("Vous n'avez pas assez de Golds \n Argent : %d \n",argent); 
  choixA=0;
  }
  if (choixA>5 && choixA!=10) {
